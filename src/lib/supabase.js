e69b69f19447dd2e40553073ab6488b29fbc601b
yogeshwaran2910 <yogeshwaran29.10.2003@email.com>
2026-04-27 14:31:03 +0530

    All changes are done

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,9 @@
-  import { supabase } from '@supabase/supabase-js'
 
-  export const supabase = createClient(
+  import { createClient } from '@supabase/supabase-js'
+
+  const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_ANON_KEY
   )
+
+  export { supabase as default }
